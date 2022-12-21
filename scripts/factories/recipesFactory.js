9f6127e9048497124321f2d19942297889e43bb7
Killouh <email@example.com>
2022-12-21 18:50:58 +0100

    Searchbar completed with duplicate error corrected

--- scripts/factories/recipesFactory.js
+++ scripts/factories/recipesFactory.js
@@ -1,77 +1,86 @@
 // Génère les cartes de recettes
- export function generateRecipeHTML(recipes) {
+export function generateRecipeHTML(recipes) {
   let html = "";
+
   for (let recipe of recipes) {
-    html += '<article class = "recipe-card">'
-    html += '<img class = "ImgPlaceholder">';
-    html += '<div class = "header-card">'
-    html += `<h3>${recipe.name}</h3>`;
-    html += `<div class = "time-card">`;
-    html += `<p>${recipe.time} min</p>`;
-    html += `<i class="far fa-clock"></i>`;
-    html += '</div>';
-    html += '</div>';
-    html += `<div class = "text-card">`;
-    html += "<ul>";
-    for (let ingredient of recipe.ingredients) {
-      html += `<li>${ingredient.ingredient}`;
-      if (ingredient.quantity) {
 
-        html += `: <span class ="unit-quantity">${ingredient.quantity}`;
+    const recipeElement = document.getElementById(recipe.id);
+    if (!recipeElement) {
+
+      html += `<article class = "recipe-card" id='${recipe.id}'>`
+      html += '<img class = "ImgPlaceholder">';
+      html += '<div class = "header-card">'
+      html += `<h3>${recipe.name}</h3>`;
+      html += `<div class = "time-card">`;
+      html += `<p>${recipe.time} min</p>`;
+      html += `<i class="far fa-clock"></i>`;
+      html += '</div>';
+      html += '</div>';
+      html += `<div class = "text-card">`;
+      html += "<ul>";
+      for (let ingredient of recipe.ingredients) {
+        html += `<li>${ingredient.ingredient}`;
+        if (ingredient.quantity) {
+
+          html += `: <span class ="unit-quantity">${ingredient.quantity}`;
+        }
+        if (ingredient.unit) {
+
+          // Remplace grammes par G
+          if (ingredient.unit.includes("grammes")) {
+            ingredient.unit = ingredient.unit.replace(/grammes/gi, 'g');
+          }
+          // Remplace cuillère à soupe par cuillère
+          if (ingredient.unit.includes("cuillère à soupe")) {
+            ingredient.unit = ingredient.unit.replace(/cuillère à soupe/gi, 'cuillères');
+          }
+          // Remplace cuillères à soupe par cuillère
+          if (ingredient.unit.includes("cuillères à soupe")) {
+            ingredient.unit = ingredient.unit.replace(/cuillères à soupe/gi, 'cuillères');
+          }
+          // Met au singulier si un seul
+          if (ingredient.quantity == 1) {
+            ingredient.unit = ingredient.unit.replace(/cuillères/gi, 'cuillère');
+          }
+          // Met au singulier si un seul
+          if (ingredient.quantity == 1) {
+            ingredient.unit = ingredient.unit.replace(/sachets/gi, 'sachet');
+          }
+          // Met au singulier si un seul
+          if (ingredient.quantity == 1) {
+            ingredient.unit = ingredient.unit.replace(/pincées/gi, 'pincée');
+          }
+          // Met au singulier si un seul
+          if (ingredient.quantity == 1) {
+            ingredient.unit = ingredient.unit.replace(/tranches/gi, 'tranche');
+          }
+          // Met au singulier si un seul
+          if (ingredient.quantity == 1) {
+            ingredient.unit = ingredient.unit.replace(/boites/gi, 'boite');
+          }
+          // Met au singulier si un seul
+          if (ingredient.quantity == 1) {
+            ingredient.unit = ingredient.unit.replace(/barquettes/gi, 'barquette');
+          }
+          html += ` ${ingredient.unit}</span>`;
+        }
+
+
+
+        html += '</li>'
       }
-      if (ingredient.unit) {
-        
-        // Remplace grammes par G
-        if (ingredient.unit.includes("grammes")) {
-          ingredient.unit = ingredient.unit.replace(/grammes/gi, 'g');
-        }
-        // Remplace cuillère à soupe par cuillère
-        if (ingredient.unit.includes("cuillère à soupe"))  {
-          ingredient.unit = ingredient.unit.replace(/cuillère à soupe/gi, 'cuillères');
-        }
-        // Remplace cuillères à soupe par cuillère
-        if (ingredient.unit.includes("cuillères à soupe"))  {
-          ingredient.unit = ingredient.unit.replace(/cuillères à soupe/gi, 'cuillères');
-        }
-        // Met au singulier si un seul
-        if (ingredient.quantity == 1) {
-          ingredient.unit = ingredient.unit.replace(/cuillères/gi, 'cuillère');
-        }
-        // Met au singulier si un seul
-        if (ingredient.quantity == 1) {
-          ingredient.unit = ingredient.unit.replace(/sachets/gi, 'sachet');
-        }
-        // Met au singulier si un seul
-        if (ingredient.quantity == 1) {
-          ingredient.unit = ingredient.unit.replace(/pincées/gi, 'pincée');
-        }
-        // Met au singulier si un seul
-        if (ingredient.quantity == 1) {
-          ingredient.unit = ingredient.unit.replace(/tranches/gi, 'tranche');
-        }
-        // Met au singulier si un seul
-        if (ingredient.quantity == 1) {
-          ingredient.unit = ingredient.unit.replace(/boites/gi, 'boite');
-        }
-        // Met au singulier si un seul
-        if (ingredient.quantity == 1) {
-          ingredient.unit = ingredient.unit.replace(/barquettes/gi, 'barquette');
-        }
-        html += ` ${ingredient.unit}</span>`;
-      }
 
-
-
-      html += '</li>'
+      html += "</ul>";
+      html += `<p class = "recipe-description">${recipe.description}</p>`;
+      html += '</div>';
+      html += '</article>'
     }
-
-    html += "</ul>";
-    html += `<p class = "recipe-description">${recipe.description}</p>`;
-    html += '</div>';
-    html += '</article>'
-
   }
+  
+
+
   return html;
 }
+
 
 
--- scripts/search.js
+++ scripts/search.js
@@ -1,6 +1,9 @@
 import { recipes } from '../../data/recipes.js'
 import { generateRecipeHTML } from './factories/recipesFactory.js'
 
+
+
+
 // Genere les cartes de recettes
 const recipeListDiv = document.getElementById("resultRecipes-container");
 const recipeHTML = generateRecipeHTML(recipes);
@@ -12,10 +15,10 @@ const searchInput = document.querySelector('#searchBar');
 const resultsContainer = document.querySelector('#resultRecipes-container');
 const noResult = document.getElementById("noResults");
 
+// Revoir les normalize (voir note Fonction)
 searchInput.addEventListener('keyup', () => {
-    const searchTerm = searchInput.value.toLowerCase().normalize('NFD');
+    const searchTerm = searchInput.value.toLowerCase();
     
-
 
     if (searchTerm.length === 0) {
 
@@ -32,8 +35,19 @@ searchInput.addEventListener('keyup', () => {
         noResult.style.display = "none";
         return;
     }
+   
 
-    const matchingRecipes = searchRecipes(searchTerm);
+    // Recherche sur la searchbar pour les ingredients et les noms de plat //
+
+    var searchTerm_normalized = searchTerm.normalize('NFD').replace(/\p{Diacritic}/gu, "");
+    const matchingRecipes = recipes.filter((recipe) => {
+
+        console.log(searchTerm_normalized);
+        return recipe.name.toLowerCase().normalize('NFD').replace(/\p{Diacritic}/gu, "").includes(searchTerm_normalized) ||
+           recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().normalize('NFD').replace(/\p{Diacritic}/gu, "").includes(searchTerm_normalized));
+           
+      });
+
 
     resultsContainer.innerHTML = '';
     if (matchingRecipes.length > 0) {
@@ -54,13 +68,18 @@ searchInput.addEventListener('keyup', () => {
 
 
 
-function searchRecipes(searchTerm) {
-    const normalizedSearchTerm = typeof searchTerm === 'string' ? searchTerm.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase() : searchTerm;
-    return recipes.filter(recipe => {
-      const nameMatch = typeof recipe.name === 'string' ? recipe.name.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase().includes(normalizedSearchTerm) : false;
-      const ingredients = recipe.ingredients.map(ingredient => typeof ingredient === 'string' ? ingredient.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase() : ingredient);
-      const ingredientMatch = ingredients.some(ingredient => ingredient.includes(normalizedSearchTerm));
-      return nameMatch || ingredientMatch;
-    });
-  }
 
+
+//   function searchRecipes(searchTerm) {
+//     const normalizedSearchTerm = typeof searchTerm === 'string' ? searchTerm.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase() : searchTerm;
+//     return recipes.filter(recipe => {
+//       const nameMatch = typeof recipe.name === 'string' ? recipe.name.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase().includes(normalizedSearchTerm) : false;
+//       const ingredients = recipe.ingredients.map(ingredient => typeof ingredient === 'string' ? ingredient.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase() : ingredient);
+//       const ingredientMatch = ingredients.some(ingredient => ingredient.includes(normalizedSearchTerm));
+//       return nameMatch || ingredientMatch;
+//     });
+//   }
+
+//test à intégrer a la fonction recherche
+    // var string_norm = string.normalize('NFD').replace(/\p{Diacritic}/gu, ""); 
+    // console.log(string_norm);
